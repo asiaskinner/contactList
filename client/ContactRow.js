64134e5a5fd8d6664e9e3f62fbad97ab77d89c91
Fulongzhou <zhoufulong89@gmail.com>
2023-06-15 15:54:57 -0400

    final commit

---
+++ client/ContactRow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactRow = (props) => {
+    const { contact, selectContact } = props; 
+
+    return (
+        <tr onClick={() => selectContact(contact.id) }>
+            <td>{contact.name}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.email}</td>
+        </tr>
+    )
+}
+
+export default ContactRow; 
--- client/index.js
+++ client/index.js
@@ -2,15 +2,42 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';  
+import SingleContact from './SingleContact';
+import ContactList from './ContactList';
 
-const contactList = [
-  {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
-  {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
-  {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
-]
+
+// const contactList = [
+//   {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
+//   {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
+//   {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
+// ]
 
 const Main = () => {
-  const [mainContacts, setMainContacts] = useState(contactList)
+  const [mainContacts, setMainContacts] = useState([]); 
+  const [selectedContact, setSelectedContact] = useState({}); 
+
+  const selectContact = async (contactId) => {
+    try {
+      const res = await axios.get(`/api/contacts/${contactId}`);
+      const fetchedContact = res.data;
+      setSelectedContact(fetchedContact); 
+    } catch (error) {
+      console.log('There was a problem making contact!'); 
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('/api/contacts'); 
+        const contacts = res.data; 
+        setMainContacts(contacts); 
+      } catch (error) {
+        console.log('There was a problem making contact!')
+      }
+    } 
+    fetchData(); 
+  }, []); 
 
   return (
     <div id="main">
@@ -18,27 +45,13 @@ const Main = () => {
         <div>Contact List</div>
       </div>
       <div id="container">
-      <table>
-  <tbody>
-          <tr>
-           <th>Name:</th>
-           <th>Phone</th>
-           <th>Email</th>
-         </tr>
-        {
-          mainContacts.map(contact => {
-            return <tr>
-            <th>{contact.name}</th>
-            <th>{contact.phone}</th>
-            <th>{contact.email}</th>
-          </tr>
-          })
-        }
-        
-
-
-  </tbody>
-</table> 
+        <div id="container"> 
+          {
+            selectedContact.id ? 
+            <SingleContact selectedContact={selectedContact} selectContact={selectContact} /> 
+            : <ContactList contacts={mainContacts} selectContact={selectContact} />
+          }
+        </div> 
       </div>
     </div>
   );
